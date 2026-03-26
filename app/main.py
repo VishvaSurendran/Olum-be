3539c9d3a71bf67b3a602fdde579e65676dcf155
Vishvavsn <vishvas.renie@gmail.com>
2026-03-26 14:16:37 +0530

    Refactor CORS configuration to use a predefined origins list

--- app/main.py
+++ app/main.py
@@ -9,10 +9,14 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Chat-With-Website Engine", version="1.0.0")
 
+origins = [
+    "https://olum.ai",
+    "https://www.olum.ai",
+]
 # Set up CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
